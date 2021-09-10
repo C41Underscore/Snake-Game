c01283a4847a49571c0cf487373f5daa1ff9aca0
C41Underscore <callum.methold@sky.com>
2021-09-10 17:58:54 +0100

    main game finished, need to do final debugging

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame as pg
 from queue import Queue
+from time import sleep
+from random import randint
 
-BOARD_SIZE = 600
+BOARD_SIZE = 400
 UNIT = 20
 
 
 class GameBoard:
 
     def __init__(self, surface):
+        self.current_marker = (None, None)
         self.surface = surface
         if BOARD_SIZE % UNIT > 0:
             print("UNIT does not evenly divide into BOARD_SIZE, unable to load game!")
@@ -17,12 +20,31 @@ class GameBoard:
             self.board.append([])
             for j in range(0, int(BOARD_SIZE/UNIT)):
                 self.board[i].append(pg.Rect(i*UNIT, j*UNIT, UNIT, UNIT))
+        self.place_marker()
 
     def place_snake(self, x, y):
-        pg.draw.rect(self.surface, (0, 0, 0), self.board[y][x])
+        if x < 0 or y < 0 or x > int(BOARD_SIZE/UNIT) or y > int(BOARD_SIZE/UNIT):
+            print("Snake went out of bounds!")
+            exit(0)
+        pg.draw.rect(self.surface, (255, 255, 255), self.board[y][x])
+        if self.current_marker == (x, y):
+            self.place_marker()
+            pg.display.update(self.board[y][x])
+            return True
+        pg.display.update(self.board[y][x])
+        return False
 
     def remove_snake(self, x, y):
-        pg.draw.rect(self.surface, (255, 255, 255), self.board[y][x])
+        pg.draw.rect(self.surface, (0, 0, 0), self.board[y][x])
+        pg.display.update(self.board[y][x])
+
+    def place_marker(self):
+        r_x = randint(1, int((BOARD_SIZE/UNIT) - 1))
+        r_y = randint(1, int((BOARD_SIZE/UNIT) - 1))
+        pg.draw.rect(self.surface, (255, 0, 0), self.board[r_y][r_x])
+        pg.display.update(self.board[r_y][r_x])
+        self.current_marker = (r_x, r_y)
+        print(self.current_marker)
 
 
 class Snake:
@@ -34,60 +56,48 @@ class Snake:
         self.board.place_snake(self.head_x, self.head_y)
         self.length = 1
         self.body = []
-        self.__moving_north = True
-        self.__moving_east = None
+        self.__direction = randint(0, 3)
 
-    def move(self):
-        self.body.insert(0, (int(self.head_x), int(self.head_y)))
-        if self.__moving_north is not None:
-            if self.__moving_north:
-                self.head_y -= 1
-            else:
-                self.head_y += 1
-        else:
-            if self.__moving_east:
-                self.head_x += 1
-            else:
+    def move(self, up, down, left, right):
+        self.body.append((int(self.head_x), int(self.head_y)))
+        if up:
+            self.head_x -= 1
+            self.__direction = 0
+        if down:
+            self.head_x += 1
+            self.__direction = 1
+        if left:
+            self.head_y -= 1
+            self.__direction = 2
+        if right:
+            self.head_y += 1
+            self.__direction = 3
+        if up == down == left == right == 0:
+            if self.__direction == 0:
                 self.head_x -= 1
+            elif self.__direction == 1:
+                self.head_x += 1
+            elif self.__direction == 2:
+                self.head_y -= 1
+            elif self.__direction == 3:
+                self.head_y += 1
+        if (self.head_x, self.head_y) in self.body:
+            print("You went over yourself!")
+            exit(0)
+        if self.board.place_snake(self.head_x, self.head_y):
+            self.length += 1
         while len(self.body) > self.length - 1:
-            loc = self.body.pop()
+            loc = self.body.pop(0)
             self.board.remove_snake(loc[0], loc[1])
-            if BOARD_SIZE % UNIT < self.head_x or self.head_x < 0:
-                exit(0)
-            if BOARD_SIZE % UNIT < self.head_y or self.head_y < 0:
-                exit(0)
-        self.board.place_snake(self.head_x, self.head_y)
-
-    def move_up(self):
-        self.__moving_north = True
-        self.__moving_east = None
-
-    def move_down(self):
-        self.__moving_north = False
-        self.__moving_east = None
-
-    def move_left(self):
-        self.__moving_north = None
-        self.__moving_east = False
-
-    def move_right(self):
-        self.__moving_north = None
-        self.__moving_east = True
 
 
 def run_game(snake):
     game_running = True
     while game_running:
+        pg.event.pump()
         keys_pressed = pg.key.get_pressed()
-        if keys_pressed[pg.K_UP]:
-            snake.move_up()
-        if keys_pressed[pg.K_DOWN]:
-            snake.move_down()
-        if keys_pressed[pg.K_LEFT]:
-            snake.move_left()
-        if keys_pressed[pg.K_RIGHT]:
-            snake.move_right()
-        snake.move()
+        snake.move(keys_pressed[pg.K_UP], keys_pressed[pg.K_DOWN], keys_pressed[pg.K_LEFT], keys_pressed[pg.K_RIGHT])
+        sleep(0.1)
 
 
 def main():
